81f3f5e1a9223bd065b274a026b860c3b85b53b3
Atila <atilarampazo@gmail.com>
2025-01-14 11:47:55 -0300

    Add authentication middleware and enhance NextAuth configuration with authorization callback

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,9 +2,14 @@ import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [GitHub,Google],
+    providers: [GitHub, Google],
     pages: {
         newUser: "/",
         signIn: "/auth/login"
+    },
+    callbacks: {
+        authorized: async ({ auth }) => {
+            return !!auth;
+        }
     }
 })
